aa7e47ea739dafe820a8d45a7c8f840a7a7d06de
jay9044 <39768156+jay9044@users.noreply.github.com>
2020-04-20 12:55:08 +0100

    first

--- module01/reverse.go
+++ module01/reverse.go
@@ -7,5 +7,18 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	res := ""
+	// starting at last word - eg. "cat" index is actually two but length is 3
+	for i := len(word) - 1; i >= 0; i-- {
+		res += string(word[i])
+	}
+	return res
 }
+
+// word := "jamal"
+// 	var res string
+
+// 	for _, r := range word {
+// 		res = string(r) + res
+// 	}
+// 	fmt.Println(res)
