7514fa8bd20d9cdd94dc908ea851436442df5f91
jay9044 <39768156+jay9044@users.noreply.github.com>
2020-04-26 19:52:09 +0100

    Update fizz_buzz.go

--- module01/fizz_buzz.go
+++ module01/fizz_buzz.go
@@ -13,13 +13,20 @@ import "fmt"
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
 func FizzBuzz(n int) {
-	if n%3 == 0 && n%5 == 0 {
-		fmt.Println("Fizz Buzz, ")
-	} else if n%3 == 0 {
-		fmt.Println("Fizz, ")
-	} else if n%5 == 0 {
-		fmt.Println("Buzz, ")
-	} else {
-		fmt.Println(n, ", ")
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Print("Fizz Buzz")
+		case i%3 == 0:
+			fmt.Print("Fizz")
+		case i%5 == 0:
+			fmt.Print("Buzz")
+		default:
+			fmt.Print(i)
+		}
+		if i != n {
+			fmt.Print(", ")
+		}
 	}
+	fmt.Println()
 }
